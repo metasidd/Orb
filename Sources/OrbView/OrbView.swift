ab531882bbfe8a36c510a10d4ceec1df0bf1dccc
Siddhant Mehta <siddhant.olive92@gmail.com>
2024-11-08 16:34:31 +0530

    Reorganize files
    Create OrbConfiguration file
    Folder restructure

--- Sources/OrbView/OrbView.swift
+++ Sources/OrbView/OrbView.swift
@@ -6,42 +6,6 @@
 //
 import SwiftUI
 
-public struct OrbConfiguration {
-    public init(
-        backgroundColors: [Color] = [.green, .blue, .pink],
-        glowColor: Color = .white,
-        coreGlowIntensity: Double = 1.0,
-        showBackground: Bool = true,
-        showWavyBlobs: Bool = true,
-        showParticles: Bool = true,
-        showGlowEffects: Bool = true,
-        showShadow: Bool = true,
-        speed: Double = 60
-    ) {
-        self.backgroundColors = backgroundColors
-        self.glowColor = glowColor
-        self.showBackground = showBackground
-        self.showWavyBlobs = showWavyBlobs
-        self.showParticles = showParticles
-        self.showGlowEffects = showGlowEffects
-        self.showShadow = showShadow
-        self.coreGlowIntensity = coreGlowIntensity
-        self.speed = speed
-    }
-    
-    var glowColor: Color
-    var backgroundColors: [Color]
-
-    var showBackground: Bool
-    var showWavyBlobs: Bool
-    var showParticles: Bool
-    var showGlowEffects: Bool
-    var showShadow: Bool
-
-    var coreGlowIntensity: Double
-    var speed: Double
-}
-
 public struct OrbView: View {
     let configuration: OrbConfiguration
 
