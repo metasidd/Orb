f07e8c588d3c63a794ad5f697d75b795d57a7ac7
Siddhant Mehta <siddhant.olive92@gmail.com>
2024-11-08 17:35:55 +0530

    Add internal init for future upgrades

--- Sources/Config/OrbConfiguration.swift
+++ Sources/Config/OrbConfiguration.swift
@@ -10,6 +10,7 @@ import SwiftUI
 public struct OrbConfiguration {
     public let glowColor: Color
     public let backgroundColors: [Color]
+    public let particleColor: Color
     
     public let showBackground: Bool
     public let showWavyBlobs: Bool
@@ -19,6 +20,30 @@ public struct OrbConfiguration {
     
     public let coreGlowIntensity: Double
     public let speed: Double
+    
+    internal init(
+        backgroundColors: [Color],
+        glowColor: Color,
+        particleColor: Color,
+        coreGlowIntensity: Double,
+        showBackground: Bool,
+        showWavyBlobs: Bool,
+        showParticles: Bool,
+        showGlowEffects: Bool,
+        showShadow: Bool,
+        speed: Double
+    ) {
+        self.backgroundColors = backgroundColors
+        self.glowColor = glowColor
+        self.particleColor = particleColor
+        self.showBackground = showBackground
+        self.showWavyBlobs = showWavyBlobs
+        self.showParticles = showParticles
+        self.showGlowEffects = showGlowEffects
+        self.showShadow = showShadow
+        self.coreGlowIntensity = coreGlowIntensity
+        self.speed = speed
+    }
     
     public init(
         backgroundColors: [Color] = [.green, .blue, .pink],
@@ -31,14 +56,17 @@ public struct OrbConfiguration {
         showShadow: Bool = true,
         speed: Double = 60
     ) {
-        self.backgroundColors = backgroundColors
-        self.glowColor = glowColor
-        self.showBackground = showBackground
-        self.showWavyBlobs = showWavyBlobs
-        self.showParticles = showParticles
-        self.showGlowEffects = showGlowEffects
-        self.showShadow = showShadow
-        self.coreGlowIntensity = coreGlowIntensity
-        self.speed = speed
+        self.init(
+            backgroundColors: backgroundColors,
+            glowColor: glowColor,
+            particleColor: .white,
+            coreGlowIntensity: coreGlowIntensity,
+            showBackground: showBackground,
+            showWavyBlobs: showWavyBlobs,
+            showParticles: showParticles,
+            showGlowEffects: showGlowEffects,
+            showShadow: showShadow,
+            speed: speed
+        )
     }
 }
